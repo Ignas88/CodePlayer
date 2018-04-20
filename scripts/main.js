42273d913e645687cfdd18fa68ba16edc35e48e4
Ignas88 <ignas.c88@gmail.com>
2018-04-21 00:04:37 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+function updateOutput () {
+
+    $('iframe').contents().find('html').html('<html><head><style type="text/css">' + $('#cssPanel').val()
+        + '</style></head><body>' + $('#htmlPanel').val() + '</body></html>');
+
+    document.getElementById('outputPanel').contentWindow.eval($('#javaScriptPanel').val());
+
+};
+
+$('.toggleButton').hover(function () {
+    
+    $(this).addClass('highlightedButton');
+
+}, function () {
+
+    $(this).removeClass('highlightedButton');
+
+});
+
+$('.toggleButton').click(function () {
+
+    $(this).toggleClass('active');
+
+    $(this).removeClass('highlightedButton');
+
+    let panelId = $(this).attr('id') + 'Panel';
+
+    $('#' + panelId).toggleClass('hidden');
+
+    let numberOfActivePanels = 4 - $('.hidden').length;
+
+    $('.panel').width(($(window).width() / numberOfActivePanels) - 10);
+
+});
+
+$('.panel').height($(window).height() - $('#header').height());
+
+$('.panel').width(($(window).width() / 2) - 10);
+
+updateOutput();
+
+$('textarea').on('change keyup paste', function() {
+
+    updateOutput();
+
+});
